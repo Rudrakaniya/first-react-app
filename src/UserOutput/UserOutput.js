a70325fd1b4adb410edea324503f6baa09327c8c
Rudra <rudra.kaniya.rk@gmail.com>
2020-12-29 20:51:24 +0530

    first commit

---
+++ src/UserOutput/UserOutput.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const userOutpot = (props) => {
+  return (
+    <div>
+      <p>Username : {props.userName}</p>
+      <p>This is my second paragraph.</p>
+    </div>
+  );
+};
+
+export default userOutpot;
